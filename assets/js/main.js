20231c19f1ddff9a04bb93e8d03e5c006e44f982
Anderson <anderson@zaruc.com.br>
2019-11-26 13:40:07 -0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+$(function(){
+    suffletext(".suffletext");
+    suffletext("h2");
+    suffletext("h5.card-title");
+    suffletext(".posts h1");
+    suffletext("ul.pagination  a");
+    
+});
+function suffletext(elemente_text){
+    var text = null;
+    $(elemente_text).mouseenter(function() {
+        $(this).data("text",$(this).text());
+        text = new ShuffleText(this);
+        text.start();
+        console.log(`iniciou ${elemente_text}`);
+    }).mouseleave(function() {
+        text.stop();
+        this.innerText = $(this).data("text"); 
+        console.log(`finalizou  ${elemente_text}`);
+    });
+}
+function shtext(element){
+    console.log("passou");
+    console.log(element);
+    console.log($( element ));
+    var text = new ShuffleText(element);
+    $(element).mouseenter(function() {
+        console.log("iniciou");
+        text.start();
+    }).mouseleave(function() {
+        text.stop();
+        element.innerText = element.dataset.text; //Evita alguns problemas
+        console.log("finalizou");
+    });
+    console.log("saiu");
+    element.innerText = element.dataset.text; //Evita alguns problemas
+}
